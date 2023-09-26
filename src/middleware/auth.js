ae930e671d3a99b2f60330f8c034a427da695bde
Penelope <freyalabram@gmail.com>
2023-09-26 11:00:44 +0200

    check user id corresponds with requested resource

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -13,5 +13,18 @@ const hasValidAuthJwt = (req,res,next) => {
   }
 }
 
+const checkId = (req, res, next) => {
+  console.log(req.params.id)
+  console.log(req.user)
+const requestedId = req.params.id
+const user = req.user
+if (user && user.id === requestedId) {
+  next();
+} else {
+  res.status(403).send('User does not have permission to access this resource'); // User's UUID does not match the requested UUID
+}
+}
+
 module.exports = { 
-  hasValidAuthJwt}
+  hasValidAuthJwt, checkId}
+
--- src/routes/users.js
+++ src/routes/users.js
@@ -6,16 +6,16 @@ const {
 deleteUser,
 updateUserAvatar } = require("../controllers/users")
 const { addVan, deleteVan } = require("../controllers/vans")
-const {hasValidAuthJwt} = require("../middleware/auth")
+const {hasValidAuthJwt, checkId} = require("../middleware/auth")
 const uploadFile = require("../middleware/uploadFile")
 
 const router = express.Router()
 router.get("/", getAllUsers)
 router.get("/:id", getUserById)
-router.put("/:id", hasValidAuthJwt, updateUserById)
-router.delete("/:id", hasValidAuthJwt, deleteUser)
-router.put("/:id/vans", hasValidAuthJwt, addVan)
-router.delete("/:id/vans/:vanid", hasValidAuthJwt, deleteVan)
-router.post("/upload-avatar", hasValidAuthJwt, uploadFile.single("avatar"), updateUserAvatar)
+router.put("/:id", hasValidAuthJwt, checkId, updateUserById)
+router.delete("/:id", hasValidAuthJwt, checkId, deleteUser)
+router.put("/:id/vans", hasValidAuthJwt, checkId, addVan)
+router.delete("/:id/vans/:vanid", hasValidAuthJwt, checkId, deleteVan)
+router.post("/:id/upload-avatar", hasValidAuthJwt, checkId, uploadFile.single("avatar"), updateUserAvatar)
 
 module.exports = router
